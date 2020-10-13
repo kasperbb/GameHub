18165726ebc33134a9fc51223602d4055fda700c
kasperbb <kasperbb98@gmail.com>
2020-10-13 16:35:11 +0200

    Modifies the footer & fixes the header on smaller screens

--- js/header.js
+++ js/header.js
@@ -7,4 +7,13 @@ const hamburgerClicked = () => {
 		hamburgerIcon : document.querySelector('#hamburger'),
 		nav           : document.querySelector('#nav'),
 	};
+
+	DOM.hamburgerIcon.addEventListener('click', event => {
+		if (!DOM.nav.classList.contains('show')) {
+			DOM.nav.classList.add('show');
+		} else {
+			DOM.nav.classList.remove('show');
+		}
+		console.log('test');
+	});
 };
